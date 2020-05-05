3c2507f47d79756158d0c73069a24adc2734f9a2
ThaliaR <thalia.rebecca@gmail.com>
2020-05-05 10:57:16 +0200

    playing around with bean scope

--- src/com/springdemo/TrackCoach.java
+++ src/com/springdemo/TrackCoach.java
@@ -21,4 +21,15 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just do it: " + fortuneService.getFortune();
     }
+
+
+    // add an init method
+    public void doMyStartup(){
+        System.out.println("TrackCoach:inside method doMyStartup");
+    }
+
+    // add a destroy method
+    public void doMyCleanup(){
+        System.out.println("TrackCoach: inside method doMyCleanup");
+    }
 }
